f7f77f6e99daae91d7214858ad2ac51785ba4a56
MMcClure11 <mcclme02@luther.edu>
2020-11-30 11:57:32 -0600

    sets up necessary code to build redux store

--- src/index.js
+++ src/index.js
@@ -4,9 +4,20 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { createStore, applyMiddleware, compose } from 'redux'
+import thunk from 'redux-thunk'
+import { Provider } from 'react-redux'
+import rootReducer from './reducers'
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)))
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={ store }>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/reducers/index.js
@@ -0,0 +1,10 @@
+
+
+import { combineReducers } from 'redux'
+
+const rootReducer = combineReducers({
+  // currentUser: currentUserReducer
+
+})
+
+export default rootReducer
