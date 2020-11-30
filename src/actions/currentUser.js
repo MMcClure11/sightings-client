a8227e732f23c2c4b31055d9736c469402f3e8fd
MMcClure11 <mcclme02@luther.edu>
2020-11-30 14:19:05 -0600

    adds login action to currentUser

---
+++ src/actions/currentUser.js
@@ -0,0 +1,29 @@
+import { 
+  SET_CURRENT_USER
+} from '../actionTypes' 
+
+export function login(credentials, history) {
+  return dispatch => {
+    return fetch("http://localhost:3000/api/v1/login", {
+      credentials: "include",
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(credentials)
+    })
+      .then(r => r.json())
+      .then(response => {
+        if (response.error) {
+          alert(response.error)
+        } else {
+          dispatch({
+            type: SET_CURRENT_USER, 
+            user: response
+          })
+          history.push('/')
+        }
+      })
+      .catch(console.log)
+  }
+}
