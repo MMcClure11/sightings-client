3813d450b9b92d53da2ba75666363b8e59a6fedc
MMcClure11 <mcclme02@luther.edu>
2020-11-30 14:26:50 -0600

    adds SET_CURRETNT_USER to reducer

--- src/reducers/currentUser.js
+++ src/reducers/currentUser.js
@@ -1,6 +1,9 @@
+import { SET_CURRENT_USER } from "../actionTypes"
 
 export function currentUserReducer(state = null, action) {
   switch(action.type) {
+    case SET_CURRENT_USER:
+      return action.user
     default:
       return state
   }
