252c7e00c0ddd4e9c07e68350500a1dbf44cdf75
MMcClure11 <mcclme02@luther.edu>
2020-11-30 14:13:38 -0600

    adds login form

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,13 +1,40 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { login } from '../actions/currentUser.js'
+
 
 class Login extends Component {
-  render(){
-    return(
+
+  state = {
+    username: '',
+    password: ''
+}
+
+onChange = e => {
+    const { name, value } = e.target
+    this.setState({[name]: value})
+}
+
+onSubmit = e => {
+    e.preventDefault()
+    this.props.login(this.state, this.props.history)
+    this.setState({
+        username: '',
+        password: ''
+    })
+}
+
+  render() {
+    return (
       <div>
-        Hello from Login
+        <form onSubmit={this.onSubmit}>
+          <input placeholder="username" value={this.state.username} name="username" type="text" onChange={this.onChange} />
+          <input placeholder="password" value={this.state.password} name="password" type="password" onChange={this.onChange} />
+          <input type="submit" value="Log In"/>
+        </form>
       </div>
     )
   }
 }
 
-export default Login
+export default connect(null, { login })(Login)
