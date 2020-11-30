de9da0860f70052e82a53057e9d27763a1d6af00
MMcClure11 <mcclme02@luther.edu>
2020-11-30 14:29:41 -0600

    adds form to Signup

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,13 +1,44 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { signup } from '../actions/currentUser.js'
+
 
 class Signup extends Component {
-  render(){
-    return(
+
+  state = {
+    username: '',
+    password: '',
+    name: '',
+
+}
+
+onChange = e => {
+    const { name, value } = e.target
+    this.setState({[name]: value})
+}
+
+onSubmit = e => {
+    e.preventDefault()
+    this.props.signup(this.state, this.props.history)
+    this.setState({
+        username: '',
+        password: '',
+        name: '',
+    })
+}
+
+  render() {
+    return (
       <div>
-        Hello from Signup
+        <form onSubmit={this.onSubmit}>
+          <input placeholder="username" value={this.state.username} name="username" type="text" onChange={this.onChange} />
+          <input placeholder="password" value={this.state.password} name="password" type="password" onChange={this.onChange} />
+          <input placeholder="name" value={this.state.name} name="name" type="text" onChange={this.onChange} />
+          <input type="submit" value="Signup"/>
+        </form>
       </div>
     )
   }
 }
 
-export default Signup
+export default connect(null, { signup })(Signup)
