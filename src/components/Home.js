7814ba36fae6550e1a76db384200a175fb650d27
MMcClure11 <mcclme02@luther.edu>
2020-11-30 14:54:48 -0600

    conditionally renders on Home login/signup or a welcome to the user if the user is logged in

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,31 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import { connect } from 'react-redux'
 
-export default class Home extends Component {
+class Home extends Component {
+
+  onClick = (url) => {
+    this.props.history.push(url)
+}
+
   render() {
     return (
       <div>
-        Hello from Home
+        { this.props.loggedIn ? <h2>Welcome, {this.props.currentUser.username}</h2> :
+        <>
+          <Link to="/signup" onClick={() => this.onClick('/signup')}>Sign Up</Link> OR <Link to="/login" onClick={() => this.onClick('/login')}>Log In</Link>
+        </>
+        }
       </div>
     )
   }
 }
 
+const mapStateToProps = state => {
+  return({
+      loggedIn: !!state.currentUser,
+      currentUser: state.currentUser
+  })
+}
+
+export default connect(mapStateToProps)(Home)
