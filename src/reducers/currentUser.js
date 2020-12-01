a388f4af13c9df519cff8b2876c6ef5ac5f5ec73
MMcClure11 <mcclme02@luther.edu>
2020-12-01 10:03:07 -0600

    adds CLEAR_CURRENT_USER to currentUser reducer

--- src/reducers/currentUser.js
+++ src/reducers/currentUser.js
@@ -1,9 +1,11 @@
-import { SET_CURRENT_USER } from "../actionTypes"
+import { SET_CURRENT_USER, CLEAR_CURRENT_USER } from "../actionTypes"
 
 export function currentUserReducer(state = null, action) {
   switch(action.type) {
     case SET_CURRENT_USER:
       return action.user
+    case CLEAR_CURRENT_USER:
+      return null
     default:
       return state
   }
