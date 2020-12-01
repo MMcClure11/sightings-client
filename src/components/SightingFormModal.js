1a57c4976271fc5cf2140d46b571c9e7bc92a011
MMcClure11 <mcclme02@luther.edu>
2020-12-01 15:09:10 -0600

    comments out modal code for now

--- src/components/SightingFormModal.js
+++ src/components/SightingFormModal.js
@@ -8,12 +8,13 @@ export default class SightingFormModal extends Component {
 
   render() {
     return (
-      <div id="myModal" className="modal" style={{display: "block"}}>
-        <div className="modal-content">
-          <span className="close">&times;</span>
-          <p>Some text in the Modal..</p>
-        </div>
-      </div>
+      <div>Some day i will be a modal</div>
+      // <div id="myModal" className="modal" style={{display: "block"}}>
+      //   <div className="modal-content">
+      //     <span className="close">&times;</span>
+      //     <p>Some text in the Modal..</p>
+      //   </div>
+      // </div>
     )
   }
 }
