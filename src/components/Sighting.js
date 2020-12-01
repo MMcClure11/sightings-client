1766cf71ad000a9874ad038d37d4161bccbf35e0
MMcClure11 <mcclme02@luther.edu>
2020-12-01 11:15:25 -0600

    adds identified to sighting card

--- src/components/Sighting.js
+++ src/components/Sighting.js
@@ -5,6 +5,7 @@ function Sighting (props) {
       <img src={props.image} alt={props.common_name} width="500" height="600"></img>
       <h2>Common Name: {props.common_name}</h2>
       <h4>Scientific Name: {props.scientific_name}</h4>
+      <p>Identified? {props.identified === true ? '✅ ': '🚫'}</p>
       <p>Date seen: {props.date}</p>
       <p>Notes: {props.notes}</p>
     </div>
