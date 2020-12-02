621324b061c192de8d5ddb386b6977de4bd5c691
MMcClure11 <mcclme02@luther.edu>
2020-12-02 10:32:16 -0600

    adds ADD_SIGHTING case to reducer

--- src/reducers/sightings.js
+++ src/reducers/sightings.js
@@ -1,5 +1,6 @@
 import {
-  GET_SIGHTINGS
+  GET_SIGHTINGS,
+  ADD_SIGHTING
 } from '../actionTypes'
 
 const initialState = []
@@ -8,6 +9,8 @@ export function sightingsReducer(state = initialState, action) {
   switch(action.type){
     case GET_SIGHTINGS:
       return action.sightings
+    case ADD_SIGHTING:
+      return [...state, action.sighting]
     default:
       return state
   }
