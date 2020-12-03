cbfe1b439eb604465d9eb47ac58fc0e3d34246ef
MMcClure11 <mcclme02@luther.edu>
2020-12-02 19:40:39 -0600

    Conditionally renders buttons if a user is logged in on home page

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,17 +1,34 @@
 import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
+import { connect } from 'react-redux'
+import { NavLink } from 'react-router-dom'
 import Logout from '../components/Logout'
 
 
-export default class NavBar extends Component {
+class NavBar extends Component {
 
   render() {
     return (
-      <div>
-        <Link to="/myprofile">Profile</Link>
-        <Link to="/sightings">All Sightings</Link>
-        <Logout />
-      </div>
+      <nav className='nav'>
+        {this.props.loggedIn ? 
+          <>
+          <div className='nav__link-container'>
+            <NavLink className='nav__link' to="/">Home</NavLink>
+            <NavLink className='nav__link' to="/myprofile">My Profile</NavLink>
+            <NavLink className='nav__link' to="/sightings">Sightings</NavLink>
+          </div>
+          <div className='nav__logout-container'>
+            <Logout /> 
+          </div>
+          </>:
+          null
+        }
+      </nav>
     )
   }
 }
+
+const mapStateToProps = state => {
+  return ({loggedIn: !!state.currentUser})
+}
+
+export default connect(mapStateToProps)(NavBar)
