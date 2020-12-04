587e9b97e7fdea847b4a45342c206949a365edd7
MMcClure11 <mcclme02@luther.edu>
2020-12-03 20:57:45 -0600

    adds heading small

--- src/components/Sighting.js
+++ src/components/Sighting.js
@@ -17,14 +17,14 @@ function Sighting (props) {
       <div className='card__side card__side--back'>
         <div className='card__content'>
           <h2 className='heading-secondary'>Category: {props.category.name}</h2>
-          <h4 className='heading-tertiary'>Scientific Name: {props.scientific_name}</h4>
-          <p>Identified? {props.identified === true ? '✅ ': '🚫'}</p>
+          <h4 className='heading-tertiary heading-tertiary--no-margin'>Scientific Name: {props.scientific_name}</h4>
+          <p className='heading-small'>Identified? {props.identified === true ? '✅ ': '🚫'}</p>
           { !props.user &&
-            <p>{props.public === true ? 'Public Sighting' : 'Private Sighting'}</p>
+            <p className='heading-small'>{props.public === true ? 'Public Sighting' : 'Private Sighting'}</p>
           }
-          <p>Location: {props.location.city}, {props.location.region}, {props.location.country}</p>
-          <p>Date seen: {props.date}</p>
-          <p>Notes: {props.notes}</p>
+          <p className='heading-small'>Location: {props.location.city}, {props.location.region}, {props.location.country}</p>
+          <p className='heading-small'>Date seen: {props.date}</p>
+          <p className='heading-small'>Notes: {props.notes}</p>
           { props.currentOwner && <button onClick={() => props.populateForm(props)}>Edit</button>}
           { props.currentOwner && <button onClick={onClick}>Delete</button>}
         </div>
