6199649b2dcf5e7e606cb7ff6b4f2e694dfd949d
MMcClure11 <mcclme02@luther.edu>
2020-12-07 20:07:54 -0600

    adds html markup to login component

--- src/components/Login.js
+++ src/components/Login.js
@@ -26,11 +26,24 @@ onSubmit = e => {
 
   render() {
     return (
-      <div>
-        <form onSubmit={this.onSubmit}>
-          <input placeholder="username" value={this.state.username} name="username" type="text" onChange={this.onChange} />
-          <input placeholder="password" value={this.state.password} name="password" type="password" onChange={this.onChange} />
-          <input className="btn btn--small" type="submit" value="Log In"/>
+      <div className='auth-form-container'>
+        <form className='auth-form u-margin-top-big' onSubmit={this.onSubmit}>
+          <div className="u-margin-bottom-medium">
+            <h2 className="heading-secondary--secondary u-margin-left-medium">
+                Login 
+            </h2>
+          </div>
+          <div className="auth-form__group">
+            <input className='auth-form__input' placeholder="username" value={this.state.username} name="username" type="text" onChange={this.onChange} />
+            <label for="username" className="auth-form__label">Username</label>
+          </div>
+          <div className="auth-form__group">
+            <input className='auth-form__input' placeholder="password" value={this.state.password} name="password" type="password" onChange={this.onChange} />
+            <label for="password" className="auth-form__label">Password</label>
+          </div> 
+          <div className="auth-form__group">
+            <input className='btn u-margin-left-medium' type="submit" value="Log In"/>
+          </div>
         </form>
       </div>
     )
