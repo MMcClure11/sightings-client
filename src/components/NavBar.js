affed4a5a4dc26fd5e8d3ff258885b49e1fc76db
MMcClure11 <mcclme02@luther.edu>
2020-12-08 13:57:39 -0600

    reimports sprites, to include binoculars for sightings

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -16,14 +16,28 @@ class NavBar extends Component {
           <div className='nav__link-container'>
             <NavLink className='nav__link' to="/">
               <span>
-                <svg className="icon icon--home3">
+                <svg className="icon icon--nav">
                   <use href={sprite + '#icon-home3'} />
                 </svg>
                 Home
               </span>
             </NavLink>
-            <NavLink className='nav__link' to="/myprofile">My Profile</NavLink>
-            <NavLink className='nav__link' to="/sightings">Sightings</NavLink>
+            <NavLink className='nav__link' to="/myprofile">
+              <span>
+                <svg className="icon icon--nav">
+                  <use href={sprite + '#icon-user'} />
+                </svg>
+                Profile
+              </span>  
+            </NavLink>
+            <NavLink className='nav__link' to="/sightings">
+            <span>
+                <svg className="icon icon--nav">
+                  <use href={sprite + '#icon-binoculars'} />
+                </svg>
+                Sightings
+              </span>
+            </NavLink>
           </div>
           <div className='nav__logout-container'>
             <Logout /> 
