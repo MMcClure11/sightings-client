531b9a2ef143e38e81d4fcbf85c76fe66fe649f0
MMcClure11 <mcclme02@luther.edu>
2020-12-11 09:27:33 -0600

    refactors navBar to functional component

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,85 +1,81 @@
-import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { NavLink } from 'react-router-dom'
 import Logout from '../components/Logout'
 import sprite from '../imgs/sprite.svg'
 
 
-class NavBar extends Component {
+const NavBar = props => {
 
-  render() {
-    return (
-      
-      <nav className='nav '>
-        {this.props.loggedIn ? 
-          <>
-          <div className='nav__link-container'>
-            <NavLink className='nav__link' to="/">
-              <span>
-                <svg className="icon icon--nav">
-                  <use href={sprite + '#icon-home3'} />
-                </svg>
-                Home
-              </span>
-            </NavLink>
-            <NavLink className='nav__link' to="/myprofile">
-              <span>
-                <svg className="icon icon--nav">
-                  <use href={sprite + '#icon-user'} />
-                </svg>
-                Profile
-              </span>  
-            </NavLink>
-            <NavLink className='nav__link' to="/sightings">
+  
+  return (
+    <nav className='nav '>
+      {props.loggedIn ? 
+        <>
+        <div className='nav__link-container'>
+          <NavLink className='nav__link' to="/">
             <span>
-                <svg className="icon icon--nav">
-                  <use href={sprite + '#icon-binoculars'} />
-                </svg>
-                Sightings
-              </span>
-            </NavLink>
-          </div>
-          <div className='nav__logout-container'>
-            <Logout /> 
-          </div>
-          </>:
-            <div className='nav__link-container'>
-              <NavLink className='nav__link' to="/">
-                <span>
-                  <svg className="icon icon--nav">
-                    <use href={sprite + '#icon-home3'} />
-                  </svg>
-                  Home
-                </span>
-              </NavLink>
-              <NavLink className='nav__link' to="/signup">
-                <span>
-                  <svg className="icon icon--nav">
-                    <use href={sprite + '#icon-quill'} />
-                  </svg>
-                  Sign Up
-                </span>
-              </NavLink>
-              <NavLink className='nav__link' to="/login">
-                <span>
-                  <svg className="icon icon--nav">
-                    <use href={sprite + '#icon-unlocked'} />
-                  </svg>
-                  Login
-                </span>  
-              </NavLink>
-            </div>
-        }
-      </nav>
-      
-    )
-  }
+              <svg className="icon icon--nav">
+                <use href={sprite + '#icon-home3'} />
+              </svg>
+              Home
+            </span>
+          </NavLink>
+          <NavLink className='nav__link' to="/myprofile">
+            <span>
+              <svg className="icon icon--nav">
+                <use href={sprite + '#icon-user'} />
+              </svg>
+              Profile
+            </span>  
+          </NavLink>
+          <NavLink className='nav__link' to="/sightings">
+          <span>
+              <svg className="icon icon--nav">
+                <use href={sprite + '#icon-binoculars'} />
+              </svg>
+              Sightings
+            </span>
+          </NavLink>
+        </div>
+        <div className='nav__logout-container'>
+          <Logout /> 
+        </div>
+        </>
+        :
+        <div className='nav__link-container'>
+          <NavLink className='nav__link' to="/">
+            <span>
+              <svg className="icon icon--nav">
+                <use href={sprite + '#icon-home3'} />
+              </svg>
+              Home
+            </span>
+          </NavLink>
+          <NavLink className='nav__link' to="/signup">
+            <span>
+              <svg className="icon icon--nav">
+                <use href={sprite + '#icon-quill'} />
+              </svg>
+              Sign Up
+            </span>
+          </NavLink>
+          <NavLink className='nav__link' to="/login">
+            <span>
+              <svg className="icon icon--nav">
+                <use href={sprite + '#icon-unlocked'} />
+              </svg>
+              Login
+            </span>  
+          </NavLink>
+        </div>
+      }
+    </nav>
+  )
 }
 
 const mapStateToProps = state => {
   return ({
     loggedIn: !!state.currentUser,
-    currentUser: state.currentUser
   })
 }
 
