f0253a86d6a94bb8bb14285c8b56dfd3c0d5813c
MMcClure11 <mcclme02@luther.edu>
2020-12-11 09:36:21 -0600

    refactors CommentFormModal, removes unnecessary code

--- src/containers/CommentFormModal.js
+++ src/containers/CommentFormModal.js
@@ -1,27 +1,18 @@
-import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { editComment } from '../actions/sightings'
 import CommentForm from '../components/CommentForm'
 
-class CommentFormModal extends Component {
+const CommentFormModal = props => {
 
-  onSubmit = (event) => {
-    event.preventDefault();
-    this.props.editComment(this.state, this.props.commentId)
-    this.props.toggle()
-  }
-
-  render() {
-    const display = this.props.display ? "block" : "none"
-    return (
-      <div id="myModal" className="modal" style={{ display }}>
-      <div className="modal__content">
-        <span  onClick={this.props.toggle} className="close">&times;</span>
-        <CommentForm commentId={this.props.commentId} toggle={this.props.toggle} />
-      </div>
+  const display = props.display ? "block" : "none"
+  return (
+    <div id="myModal" className="modal" style={{ display }}>
+    <div className="modal__content">
+      <span  onClick={props.toggle} className="close">&times;</span>
+      <CommentForm commentId={props.commentId} toggle={props.toggle} />
     </div>
-    )
-  }
+  </div>
+  )
 }
 
 export default connect(null, { editComment })(CommentFormModal)
