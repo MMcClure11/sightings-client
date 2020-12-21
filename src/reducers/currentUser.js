4d970644f9e80f825e5a7df4976c38c7e85193b8
MMcClure11 <mcclme02@luther.edu>
2020-12-21 17:16:08 -0600

    Refactor currentUserReducer to not check for Bening or Complete auth

--- src/reducers/currentUser.js
+++ src/reducers/currentUser.js
@@ -5,20 +5,31 @@ import {
   COMPLETE_AUTH
 } from "../actionTypes"
 
-export function currentUserReducer(state = { currentUser: {}, authComplete: false}, action) {
+export function currentUserReducer(state = null, action) {
   switch(action.type) {
     case SET_CURRENT_USER:
-      return {
-        ...state,
-        currentUser: action.user,
-      }
+      return action.user
     case CLEAR_CURRENT_USER:
       return null
-    case BEGIN_AUTH:
-      return { ...state, authComplete: false }
-    case COMPLETE_AUTH:
-      return { ...state, authComplete: true}
     default:
       return state
   }
 }
+
+// export function currentUserReducer(state = { currentUser: {}, authComplete: false}, action) {
+//   switch(action.type) {
+//     case SET_CURRENT_USER:
+//       return {
+//         ...state,
+//         currentUser: action.user,
+//       }
+//     case CLEAR_CURRENT_USER:
+//       return null
+//     case BEGIN_AUTH:
+//       return { ...state, authComplete: false }
+//     case COMPLETE_AUTH:
+//       return { ...state, authComplete: true}
+//     default:
+//       return state
+//   }
+// }
