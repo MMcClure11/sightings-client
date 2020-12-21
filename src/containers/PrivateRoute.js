b93d9420439887bad3af2bcb4c384504876dd6f0
MMcClure11 <mcclme02@luther.edu>
2020-12-21 17:30:33 -0600

    Update PrivateRoute to not use authUser for now

--- src/containers/PrivateRoute.js
+++ src/containers/PrivateRoute.js
@@ -1,38 +1,55 @@
-import React, { Component } from 'react'
+// import React, { Component } from 'react'
 import { connect } from "react-redux"
-import { authUser } from '../actions/currentUser'
+// import { authUser } from '../actions/currentUser'
 import { Route, Redirect} from 'react-router-dom' 
 
-class PrivateRoute extends Component {
+const PrivateRoute = ({ component: Component,loggedIn, ...rest }) => {
 
-  componentDidMount() {
-    this.props.authUser()
-  }
-
-  renderContent = props => {
-    const { component: Component, currentUser } = this.props
-    if (currentUser && currentUser.authComplete) {
-      if (currentUser && !currentUser.currentUser.username) {
-        return <Redirect to='/' />
-      } else {
-        return <Component {...props} />
-      }
-    }
-  }
-
-  render() {
-    const { component: Component, currentUser, authUser, ...rest} = this.props
-    return <Route {...rest} render={(props) => (
-      this.renderContent(props)
-    )} />
-  }
+  return <Route {...rest} render={(props) => (
+    loggedIn
+      ? <Component {...props} />
+      : <Redirect to='/' />
+  )}
+  />
 }
 
 const mapStateToProps = (state) => {
-  return { 
-    currentUser: state.currentUser 
-  }
+  return { loggedIn: !!state.currentUser }
 }
 
+export default connect(mapStateToProps)(PrivateRoute)
 
-export default connect(mapStateToProps, { authUser })(PrivateRoute)
+// PrivateRoute component using authUser
+// class PrivateRoute extends Component {
+
+//   componentDidMount() {
+//     this.props.authUser()
+//   }
+
+//   renderContent = props => {
+//     const { component: Component, currentUser } = this.props
+//     if (currentUser && currentUser.authComplete) {
+//       if (currentUser && !currentUser.currentUser.username) {
+//         return <Redirect to='/' />
+//       } else {
+//         return <Component {...props} />
+//       }
+//     }
+//   }
+
+//   render() {
+//     const { component: Component, currentUser, authUser, ...rest} = this.props
+//     return <Route {...rest} render={(props) => (
+//       this.renderContent(props)
+//     )} />
+//   }
+// }
+
+// const mapStateToProps = (state) => {
+//   return { 
+//     currentUser: state.currentUser 
+//   }
+// }
+
+
+// export default connect(mapStateToProps, { authUser })(PrivateRoute)
